7ececfecc4af24346cd345f686a0082e993449fc
szymonsobczyk <szymon1976@gmail.com>
2023-08-29 11:39:20 +0200

    Add project files.

---
+++ Dictionary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dictionary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
